673da3491318bdbfbc95567421752d95d2f6f26c
Matt Macosko <matt@inneedhemp.com>
2025-07-28 07:14:59 -0700

    Initial Commit

---
+++ Vision Builder/Vision_BuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vision_BuilderApp.swift
+//  Vision Builder
+//
+//  Created by matthew macosko on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Vision_BuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Vision BuilderTests/Vision_BuilderTests.swift
@@ -0,0 +1,17 @@
+//
+//  Vision_BuilderTests.swift
+//  Vision BuilderTests
+//
+//  Created by matthew macosko on 7/28/25.
+//
+
+import Testing
+@testable import Vision_Builder
+
+struct Vision_BuilderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
